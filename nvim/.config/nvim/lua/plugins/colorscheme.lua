5bf2b27a6872f4b755d4d40bcf2f1062b8d6d9c8
hugo <h.domingos@ua.pt>
2025-01-18 12:45:59 +0000

    Initial Commit with tmux|zshrc|terminator|nvim configs

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- configure lazyvim to load gruvbox
+--   {
+--     "lazyvim/lazyvim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+return {
+  -- add gruvbox
+  { "wittyjudge/gruvbox-material.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+      {
+        "folke/lazydev.nvim",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      require("lspconfig").lua_ls.setup({ capabilites = capabilities })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local c = vim.lsp.get_client_by_id(args.data.client_id)
+          if not c then
+            return
+          end
+
+          if vim.bo.filetype == "lua" then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+              end,
+            })
+          end
+        end,
+      })
+
+      require("lspconfig").rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {
+            diagnostics = {
+              enable = true,
+            },
+          },
+        },
+      })
+
+      -- Python LSP configuration
+      require("lspconfig").pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic", -- or "strict" for stricter checks
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
